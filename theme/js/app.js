4673f696c9bafd5fcc7536d2ac1f2edc20a7007d
Osman Gormus <osman@gorm.us>
2017-12-18 10:47:11 -0800

    simplified placeholder page
    Removed old `GitHub Pages`.

--- theme/js/app.js
+++
@@ -1,6 +0,0 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
-
-// Open external links in new tabs/windows.
-$("a[rel=external]").attr("target", "_blank");
